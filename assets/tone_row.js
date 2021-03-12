c06835d50e7c3bd9fd2887e1f5a98f31101df440
Ryan <ryanpburnett@yahoo.com>
2021-03-12 12:17:16 -0600

    tone row shuffling using Fisher Yates method

--- assets/tone_row.js
+++ assets/tone_row.js
@@ -1,4 +1,4 @@
-const pitchClass = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+const pitchClass = [" C ", " C# ", " D ", " D# ", " E ", " F ", " F# ", " G ", " G# ", " A ", " A# ", " B "]
 
 const gen1 = document.getElementById("gen1")
 
@@ -7,12 +7,24 @@ gen1.addEventListener("click", generateRow)
 function generateRow() {
     document.querySelector("#tone-row").innerText = ""
 
-    for (let i = 0; i < 12; i++) {
-        const rowElement = document.createElement("p")
-
-        rowElement.innerText = pitchClass[Math.floor(Math.random() * pitchClass.length)]
-        
-        document.getElementById("tone-row").appendChild(rowElement)
+    // Fisher-Yates array shuffle
+    let i = pitchClass.length, j, temp
+    while(i > 0, i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = pitchClass[j];
+    pitchClass[j] = pitchClass[i];
+    pitchClass[i] = temp;
     }
+
+    document.querySelector("#tone-row").innerText = pitchClass
+
+    // // old randomizer
+    // for (let i = 0; i < 12; i++) {
+    //     const rowElement = document.createElement("p")
+
+    //     rowElement.innerText = pitchClass[Math.floor(Math.random() * pitchClass.length)]
+        
+    //     document.getElementById("tone-row").appendChild(rowElement)
+    // }
 
 }
