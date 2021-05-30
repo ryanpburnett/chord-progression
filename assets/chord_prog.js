0befa8babd8dfe7f892d3dc69855e6918a17fb91
Ryan <ryanpburnett@yahoo.com>
2021-05-30 12:46:21 -0500

    added 7ths, fixed problem where chords persisted in chordType array

--- assets/chord_prog.js
+++ assets/chord_prog.js
@@ -1,17 +1,48 @@
 const pitchClass = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-const chordType = []
 
 const gen2 = document.getElementById("gen2")
 const numOfChords = document.getElementById("numOfChords")
+
 const major = document.getElementById("major")
 const minor = document.getElementById("minor")
 const dim = document.getElementById("dim")
 const aug = document.getElementById("aug")
 
+const domSeven = document.getElementById("domSeven")
+const majSeven = document.getElementById("majSeven")
+const minSeven = document.getElementById("minSeven")
+
 gen2.addEventListener("click", generateProg)
+
+const sevenths = document.getElementById("sevenths")
+const more = document.getElementById("more")
+
+let mode = "hide"
+let mode2 = "hide"
+
+function seventhsShow() {
+    if (mode === "hide") {
+        mode = "show"
+        sevenths.setAttribute("class", "show")
+    }else{
+        mode = "hide"
+        sevenths.setAttribute("class", "hide")
+    }
+}
+
+function moreShow() {
+    if (mode2 === "hide") {
+        mode2 = "show"
+        more.setAttribute("class", "show")
+    }else{
+        mode2 = "hide"
+        more.setAttribute("class", "hide")
+    }
+}
 
 function generateProg() {
     document.querySelector("#chord-prog").innerText = ""
+    chordType = []
 
     for (let i = 0; i < major.value; i++) {
         chordType.push("")
@@ -27,6 +58,18 @@ function generateProg() {
 
     for (let i = 0; i < aug.value; i++) {
         chordType.push("aug")
+    }
+
+    for (let i = 0; i < domSeven.value; i++) {
+        chordType.push("7")
+    }
+
+    for (let i = 0; i < majSeven.value; i++) {
+        chordType.push("M7")
+    }
+
+    for (let i = 0; i < minSeven.value; i++) {
+        chordType.push("m7")
     }
 
     for (let i = 0; i < numOfChords.value; i++) {
