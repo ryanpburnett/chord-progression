eeebd1f4c9707385a82fb1ff9b4a6ae3f957b4f3
Ryan <ryanpburnett@yahoo.com>
2021-05-30 13:18:11 -0500

    sliders on rhythm matrix generator

--- assets/rhythm_matrix.js
+++ assets/rhythm_matrix.js
@@ -1,14 +1,24 @@
-const rhythmMatrix = ["-", "X"]
-
 const numOfBeats = document.getElementById("numOfBeats")
 
 const gen3 = document.getElementById("gen3")
+
+const notes = document.getElementById("notes")
+const rests = document.getElementById("rests")
 
 gen3.addEventListener("click", generateMatrix)
 // fix this with CSS grid? clearfix?
 // rearrange array instead of randomizing so as not to repeat pitch classes
 function generateMatrix() {
     document.querySelector("#rhythm-matrix").innerText = ""
+    const rhythmMatrix = []
+
+    for (let i = 0; i < notes.value; i++) {
+        rhythmMatrix.push("X")
+    }
+
+    for (let i = 0; i < rests.value; i++) {
+        rhythmMatrix.push("-")
+    }
 
     for (let i = 0; i < numOfBeats.value; i++) {
 
